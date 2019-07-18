d80d597e72714c235626a1f49f718103a4784ee5
Veronica Lopez <nicaa0695@gmail.com>
2019-07-18 19:04:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  i = 0 
+  new_collection = []
+  while i < array.length 
+  new_collection << yield(array[i])
+  i += 1 
+  puts #{i.split(" ".first}
+end
+new_collection
+end
